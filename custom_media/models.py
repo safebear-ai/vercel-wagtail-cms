a3a967f999aafd45927c72fa239e217ef8a377eb
LeoLChalot <l.larouchalot@gmail.com>
2024-11-13 11:05:30 +0100

    Update upload logic to avoid local files

--- custom_media/models.py
+++ custom_media/models.py
@@ -13,20 +13,25 @@ import os
 import requests
 from django.conf import settings
 
-BLOB_READ_WRITE_TOKEN = os.getenv('BLOB_READ_WRITE_TOKEN', 'vercel_blob_rw_gQb3DHG6aJKWElj6_A1CzC7NbP3Asf3I6t5Zccj8mWPXfpS')
+BLOB_READ_WRITE_TOKEN = os.getenv(
+    "BLOB_READ_WRITE_TOKEN",
+    "vercel_blob_rw_gQb3DHG6aJKWElj6_A1CzC7NbP3Asf3I6t5Zccj8mWPXfpS",
+)
 VERCEL_BLOB_API_URL = "https://api.vercel.com/v1/blobs"
+
 
 class CustomImage(AbstractImage):
     """
     Modèle personnalisé pour gérer l'upload d'image en utilisant le Blob Store de Vercel.
     """
+
     blob_url = models.URLField(blank=True, null=True)
 
     tags = models.ManyToManyField(
-        'taggit.Tag',
-        through='custom_media.CustomImageTag',
-        related_name='custom_media_images',  # Change related_name to make it unique
-        blank=True
+        "taggit.Tag",
+        through="custom_media.CustomImageTag",
+        related_name="custom_media_images",  # Change related_name to make it unique
+        blank=True,
     )
 
     uploaded_by_user = models.ForeignKey(
@@ -35,52 +40,60 @@ class CustomImage(AbstractImage):
         blank=True,
         editable=False,
         on_delete=models.SET_NULL,
-        related_name='custom_media_uploaded_images'  # Change related_name to make it unique
+        related_name="custom_media_uploaded_images",  # Change related_name to make it unique
     )
 
+
     def save(self, *args, **kwargs):
-        # Sauvegarde de base pour que le fichier soit accessible
+        # Envoi du fichier à l'API Blob de Vercel
+        if not self.blob_url:  # Éviter les doubles uploads
+            with self.file.open("rb") as f:
+                files = {"file": (self.file.name, f, self.file.file.content_type)}
+                headers = {"Authorization": f"Bearer {BLOB_READ_WRITE_TOKEN}"}
+
+                response = requests.post(
+                    VERCEL_BLOB_API_URL, headers=headers, files=files
+                )
+
+                if response.status_code == 200:
+                    blob_data = response.json()
+                    self.blob_url = blob_data.get("url")
+                else:
+                    raise Exception(
+                        f"Erreur lors de l'upload sur Vercel Blob Store: {response.status_code} {response.text}"
+                    )
+
+        # Sauvegarder les métadonnées (URL blob) dans la base de données
         super().save(*args, **kwargs)
 
-        # Envoi du fichier à l'API Blob de Vercel
-        with self.file.open('rb') as f:
-            files = {'file': (self.file.name, f, self.file.file.content_type)}
-            headers = {
-                'Authorization': f'Bearer {BLOB_READ_WRITE_TOKEN}'
-            }
+    admin_form_fields = Image.admin_form_fields + ("blob_url",)
 
-            response = requests.post(VERCEL_BLOB_API_URL, headers=headers, files=files)
-
-            if response.status_code == 200:
-                blob_data = response.json()
-                self.blob_url = blob_data.get("url")
-                # Sauvegarder de nouveau l'URL du blob
-                super().save(*args, **kwargs)
-            else:
-                raise Exception(f"Erreur lors de l'upload sur Vercel Blob Store: {response.status_code} {response.text}")
-
-    admin_form_fields = Image.admin_form_fields + ('blob_url',)
 
 class CustomImageTag(models.Model):
     tag = models.ForeignKey(
-        'taggit.Tag',
-        related_name='custom_media_image_tags',  # Change related_name to make it unique
-        on_delete=models.CASCADE
+        "taggit.Tag",
+        related_name="custom_media_image_tags",  # Change related_name to make it unique
+        on_delete=models.CASCADE,
     )
     content_object = models.ForeignKey(
         CustomImage,
-        related_name='custom_media_tagged_items',  # Change related_name to make it unique
-        on_delete=models.CASCADE
+        related_name="custom_media_tagged_items",  # Change related_name to make it unique
+        on_delete=models.CASCADE,
     )
+
 
 class CustomRendition(AbstractRendition):
     """
     Classe pour gérer les rendus d'images personnalisés.
     """
-    image = models.ForeignKey(CustomImage, on_delete=models.CASCADE, related_name='renditions')
+
+    image = models.ForeignKey(
+        CustomImage, on_delete=models.CASCADE, related_name="renditions"
+    )
 
     class Meta:
-        unique_together = (('image', 'filter_spec', 'focal_point_key'),)
+        unique_together = (("image", "filter_spec", "focal_point_key"),)
+
 
 class CustomDocument(AbstractDocument):
     # Ajoutez des champs personnalisés ici si nécessaire
