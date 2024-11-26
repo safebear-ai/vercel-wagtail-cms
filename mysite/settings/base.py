b7da2133cfb3a82ddeea91fbfae10b83db4423e9
LeoLChalot <l.larouchalot@gmail.com>
2024-11-26 10:12:59 +0100

    backend

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -164,7 +164,7 @@ STORAGES = {
         "BACKEND": DEFAULT_FILE_STORAGE,
     },
     "staticfiles": {
-        "BACKEND": 'storage_backends.vercel_blob_storage.VercelBlobStorage',
+        "BACKEND": 'storage_backend.vercel_blob_storage.VercelBlobStorage',
     },
 }
 
