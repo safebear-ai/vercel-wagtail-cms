d8b02ad44e9252ed12d56b8009bf6823f8dd9367
LeoLChalot <l.larouchalot@gmail.com>
2024-11-27 14:04:44 +0100

    update vercel build configuration

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+echo "Building Project Packages..."
 pip install -r requirements.txt
+
+echo "Migrating Database..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting Static Files..."
 python manage.py collectstatic --no-input
